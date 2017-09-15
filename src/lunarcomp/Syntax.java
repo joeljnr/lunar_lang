72a2cedee153e1d846a95a5945c90786fba66b6b
joeljnr <joel_asj@outlook.com>
2017-09-15 11:19:07 -0300

    metodo erro criado

--- src/lunarcomp/Syntax.java
+++ src/lunarcomp/Syntax.java
@@ -6,8 +6,16 @@ import static lunarcomp.LunarComp._erros;
 public class Syntax {
     
     private ArrayList<Token> tokens;
+    private int pos;
     
-    public boolean termo(int pos) {
+    //melhorar o estado seguro
+    public void erro(String token, int linha, String descr) {
+        _erros.add(new Erro(token, linha, descr));
+        while(!tokens.get(pos).getToken().equals("\n"))
+            pos++;
+    }
+    
+    public boolean termo() {
         Token t = tokens.get(pos);
         if(t.getToken().equals("T_ID") || t.getToken().equals("T_NUM") || t.getToken().equals("T_BOOL")  || t.getToken().equals("T_CHAR") || 
                 t.getToken().equals("T_STRING")) 
@@ -16,43 +24,43 @@ public class Syntax {
             return false;      
     }
     
-    public boolean exp_unaria(int pos) {
+    public boolean exp_unaria() {
         Token t = tokens.get(pos);
-        if(termo(pos))
+        if(termo())
             return true;
         else if(t.getToken().equals("T_ID")) {
             t = tokens.get(++pos);
             if(t.getToken().equals("T_OPU"))
                 return true;
             else {
-                _erros.add(new Erro(t.getToken(), t.getLinha(), "Operador unário esperado"));
+                erro(t.getToken(), t.getLinha(), "Operador unário esperado");
                 return false;
             }
         }
-        _erros.add(new Erro(t.getToken(), t.getLinha(), "Id ou termo esperado"));
+        erro(t.getToken(), t.getLinha(), "Id ou termo esperado");
         return false;
     }
     
-    public boolean exp_aritmetica(int pos) {
+    public boolean exp_aritmetica() {
         Token t = tokens.get(pos), t2 = tokens.get(pos+1);
-        if(exp_unaria(pos) && !t2.getToken().equals("T_OPA"))
+        if(exp_unaria() && !t2.getToken().equals("T_OPA"))
             return true;
-        else if(exp_unaria(pos)) {
+        else if(exp_unaria()) {
             t = tokens.get(++pos);
             if(t.getToken().equals("T_OPA")) {
                 t = tokens.get(++pos);
-                if(exp_aritmetica(pos)) {
+                if(exp_aritmetica()) {
                     return true;
                 } else {
-                    _erros.add(new Erro(t.getToken(), t.getLinha(), "Expressão aritimética esperada"));
+                    erro(t.getToken(), t.getLinha(), "Expressão aritimética esperada");
                     return false;
                 }
             } else {
-                _erros.add(new Erro(t.getToken(), t.getLinha(), "Operador aritimético esperado"));
+                erro(t.getToken(), t.getLinha(), "Operador aritimético esperado");
                 return false;
             }   
         } else {
-            _erros.add(new Erro(t.getToken(), t.getLinha(), "Expressão unária esperada"));
+            erro(t.getToken(), t.getLinha(), "Expressão unária esperada");
             return false;
         }
     }
